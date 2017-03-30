755bdaf7b4d87f02bc62ea4d17f077a68f6fa82a
Alesh Houdek <mail@alesh.com>
2017-03-30 06:43:04 -0600

    serving a file

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 var http = require('http');
+var fs = require('fs');
 
 var server = http.createServer(function(req, res) {
   console.log('Responding to a request.');
-  res.end('<h1>Hello, World!</h1>');
+  
+  // res.end('<h1>Hello, World</h1>');
+
+  fs.readFile('app/index.html', function(err, data) {
+    res.end(data);
+  })
 });
 server.listen(3000);

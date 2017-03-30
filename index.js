0d34487cc70c16f7a1b79bae8c5d1914a21c9195
Alesh Houdek <mail@alesh.com>
2017-03-30 06:56:55 -0600

    resolving file request

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 var http = require('http');
-var fs = require('fs');
+var fs   = require('fs');
+var path = require('path');
 
 var server = http.createServer(function(req, res) {
   console.log('Responding to a request.');
-  
+
   // res.end('<h1>Hello, World</h1>');
 
-  fs.readFile('app/index.html', function(err, data) {
+
+  var url = req.url;
+
+  var fileName = 'index.html';
+  if (url.length > 1) {
+    fileName = url.substring(1);
+  }
+
+  console.log(fileName);
+
+  var filePath = path.resolve(__dirname, 'app', fileName);
+  fs.readFile(filePath, function(err, data) {
     res.end(data);
-  })
+  });
+
 });
 server.listen(3000);

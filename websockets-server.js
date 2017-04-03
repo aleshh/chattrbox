9b8a412ec4dbb104111b1073d4dbea4a78fd0940
Alesh Houdek <mail@alesh.com>
2017-04-03 14:27:03 -0600

    chat server, relaying all messages

--- websockets-server.js
+++ websockets-server.js
@@ -4,13 +4,24 @@ var ws = new WebSocket.Server({
   port: 3001
 });
 
+var messages = [];
+
 console.log('websockets server started');
 
 ws.on('connection', function(socket) {
   console.log('client connection establisehd');
 
+  messages.forEach(function(msg) {
+    socket.send(msg);
+  });
+
   socket.on('message', function(data) {
     console.log('messsage received: ' + data);
-    socket.send(data);
+    messages.push(data);
+    ws.clients.forEach(function(clientSocket) {
+      clientSocket.send(data);
+    });
+
+    // socket.send(data);
   });
 });

eddf97a4ea87242c99bab76d5ba32647832899a7
Alesh Houdek <mail@alesh.com>
2017-04-06 06:23:29 -0600

    web sockets handler constructors

--- app/scripts/src/ws-client.js
+++ app/scripts/src/ws-client.js
@@ -5,11 +5,20 @@ function init(url) {
   console.log('connecting...');
 }
 
-// function registerOpenHandler(handlerFunction) {
-//   socket.onopen = () => {
-//
-//   }
-// }
+function registerOpenHandler(handlerFunction) {
+  socket.onopen = () => {
+    console.log('open');
+    handlerFunction();
+  };
+}
+
+function registerMessageHandler(handlerFunction) {
+  socket.onmessage = (e) => {
+    console.log('message', e.data);
+    let data = JSON.parse(e.data);
+    handlerFunction(data);
+  };
+}
 
 export default {
   init,
